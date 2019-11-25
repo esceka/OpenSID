03074e902291fe9eeb4a4d9a6977c914c71417fa
fusionid <zeppyshiddiq@gmail.com>
2019-11-25 18:14:21 +0700

    Draft Awal Fitur Layanan Permohonan Surat - 25/11/2019

--- donjo-app/models/Lapor_model.php
+++ donjo-app/models/Lapor_model.php
@@ -15,14 +15,16 @@
 	 */
 	public function insert()
 	{
-		$data['komentar'] = strip_tags($_POST["komentar"]);
+		//$data['komentar'] = strip_tags($_POST["komentar"]);
+		$data['komentar'] = strip_tags(" Permohonan Surat ".$_POST["nama_surat"]." - ".$_POST["hp"]." - ".$_POST["komentar"]);
 		/** ambil dari data session saja */
 		$data['owner'] = $_SESSION['nama'];
 		$data['email'] = $_SESSION['nik'];
 
 		// load library form_validation
 		$this->load->library('form_validation');
-		$this->form_validation->set_rules('komentar', 'Laporan', 'required');
+		//$this->form_validation->set_rules('komentar', 'Laporan', 'required');
+		$this->form_validation->set_rules('komentar', 'Laporan');
 		$this->form_validation->set_rules('owner', 'Nama', 'required');
 		$this->form_validation->set_rules('email', 'NIK', 'required');
 
@@ -41,6 +43,22 @@
 		if (!$outp)
 			$_SESSION['success'] = -1;
 		return ($_SESSION['success'] == 1);
+	}
+
+	public function get_dokumen_mandiri($nama_surat)
+	{
+		$this->db->select('nama_surat')
+		         ->from('ref_dokumen_mandiri')
+		         ->join('dokumen_mandiri','ref_dokumen_mandiri.id = dokumen_mandiri.ref_dokumen_mandiri_id')
+		         ->join('tweb_surat_format','tweb_surat_format.id = dokumen_mandiri.tweb_surat_format_id')
+		         ->where('tweb_surat_format.nama',$nama_surat);
+		$query = $this->db->get();
+		$data = $query->result_array();
+		for ($i=0; $i<count($data); $i++)
+		{
+			$data[$i]['nama_surat']=($i+1).") ".$data[$i]['nama_surat'];
+		}
+		return $data;
 	}
 
 }
